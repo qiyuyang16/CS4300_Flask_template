0b302f41312dbd8f150f7ee80a81425c3ccc1c72
Erik Ossner <eco9@cornell.edu>
2021-04-11 04:32:26 -0400

    File URL tweaks

--- analysis_scripts/analysis.py
+++ analysis_scripts/analysis.py
@@ -75,7 +75,7 @@ def make_wordcount_hist(corpus):
     tokens = tokenize(corpus)
     uniques, counts = np.unique(tokens, return_counts = True)
     plt.bar(uniques[:20], counts[:20], align='center')
-    plt.savefig('../app/static/hist.png', dpi=600)
+    plt.savefig('hist.png', dpi=600)
     plt.show()
 
 #if __name__ == "__main__":
