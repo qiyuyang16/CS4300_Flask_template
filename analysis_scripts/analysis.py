906415b217091a37d0f5093218b20efd03ec5a64
qiyuyang16 <jamesyang00000@outlook.com>
2021-04-11 12:06:30 -0400

    nltk stopwords bug

--- analysis_scripts/analysis.py
+++ analysis_scripts/analysis.py
@@ -3,7 +3,6 @@ import re
 import string
 import numpy as np
 import matplotlib.pyplot as plt
-from nltk.corpus import stopwords
 #from flask import app
 import os
 
@@ -132,7 +131,26 @@ def clean_tokens(tokens):
     output: list of words where useless tokens are removed
     '''
     # TODO more thorough
-    stopwords_set = set(stopwords.words('english'))
+    stopwords_set = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
+     'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
+     'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 
+     'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 
+     'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 
+     'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has',
+      'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but',
+       'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with',
+        'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',
+         'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over',
+          'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 
+          'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 
+          'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 
+          'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should',
+           "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
+            "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 
+            'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't",
+             'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 
+             'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren',
+              "weren't", 'won', "won't", 'wouldn', "wouldn't"])
     result = [w for w in tokens if w.isalpha()] # remove non-letter tokens
     result = [w for w in result if len(w) >= 2] # remove tokens below a certain length
     result = [w for w in result if w not in stopwords_set] # remove stopwords
