225a024373bfe63ef56b76f91cf5ee6ce4ad718e
Erik Ossner <eco9@cornell.edu>
2021-04-25 12:02:45 -0400

    Working multipage App

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 from multiapp import MultiApp
 import v1_app
-#import v2_app
+import v2_app
 
 app = MultiApp()
+st.set_page_config(layout="wide")
+st.image('logo.png')
 
-st.markdown("""
-# Multi-Page App
-""")
 
 # Add all your application here
 app.add_app("Version 1", v1_app.app)
-#app.add_app("Version 2", v2_app.app)
+app.add_app("Version 2", v2_app.app)
 app.run()
 #app.add_app("Model", model.app)
 # The main app
