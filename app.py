a44d24adeebaadb5162490bd2f8f4997bfd574e5
Erik Ossner <eco9@cornell.edu>
2021-04-25 11:11:14 -0400

    Multipage Apps

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown("""
 
 # Add all your application here
 app.add_app("Version 1", v1_app.app)
-app.add_app("Version 2", v2_app.app)
+#app.add_app("Version 2", v2_app.app)
 app.run()
 #app.add_app("Model", model.app)
 # The main app
